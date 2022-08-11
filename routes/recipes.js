c56ceb4c814e129b32ba0f8fc09b942bb4cfdd53
dianap321 <dianap@post.bgu.ac.il>
2022-08-11 17:14:19 +0300

    updated search

--- routes/recipes.js
+++ routes/recipes.js
@@ -24,7 +24,7 @@ router.get("/random", async (req, res, next) => {
 
 console.log("before search");
 
-router.get("/search", async (req, res, next) => {
+router.get("/search/", async (req, res, next) => {
   try{
   const {searchQuery, num, cuisine, diet, intolerances} = req.query;
   // set search params
@@ -54,7 +54,6 @@ router.get("/search", async (req, res, next) => {
 }
 })
 ;
-console.log("before info");
 
 /**
  * This path returns a full details of a recipe by its id
@@ -112,6 +111,9 @@ console.log("after info");
  * Authenticate POST requests by middleware
  */
  router.use(async function (req, res, next) {
+  console.log(req.session);
+  console.log(req.session.user_id);
+
   if (req.session && req.session.user_id) {
     DButils.execQuery("SELECT user_id FROM users").then((users) => {
       if (users.find((x) => x.user_id === req.session.user_id)) {
@@ -126,6 +128,7 @@ console.log("after info");
 
 
 router.post("/createRecipe", async (req, res, next) => {
+  console.log("in create recipe")
   try {
     // parameters exists
     // valid parameters
